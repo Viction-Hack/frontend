c42c19d29ab579db69ca311951145496835ba157
sm-stack <jeon0015@naver.com>
2023-12-25 13:36:14 +0900

    Add scratch of minting functionality

---
+++ src/components/token/MintBox.tsx
@@ -0,0 +1,23 @@
+// TODO: Fix box to enable minting of VUSD
+export default function MintBox() {
+  return (
+    <div className="flex flex-col justify-center items-center mx-auto mb-3">
+      <div className="bg-white shadow-lg overflow-hidden sm:rounded-lg p-6 m-3">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Your Wallet</h3>
+        </div>
+        <div className="border-t border-gray-200">
+          <dl>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-2 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">VUSD in Your Wallet</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-start-3">$1,500,000</dd>
+            </div>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Your Collateral</dt>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/token/index.tsx
+++ src/components/token/index.tsx
@@ -1,69 +1,34 @@
-import Link from "next/link";
+import { useState } from 'react';
+import MintBox from './MintBox';
 
 export default function Token() {
+  const [isMinting, setIsMinting] = useState(true);
+
+  const handleMintOrRedeem = () => {
+    setIsMinting(!isMinting);
+  }
+
   return (
-    <main className="flex-1 flex-row justify-center items-center min-h-screen">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <div className="container mx-auto px-4 md:px-6">
+    <>
+      <main className="bg-green-200  flex-1 flex-row justify-center items-center min-h-screen p-4 md:p-6">
+        <section className="w-full py-12 md:py-2 lg:py-3 xl:py-4">
+          <div className="container mx-auto px-4 md:px-6 mb-10">
             <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  DeltaNeutral Stablecoin
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  The perfect blend of traditional finance and web3. Experience the stability of a delta-neutral
-                  stablecoin.
-                </p>
-              </div>
-              <div className="space-x-4">
-                <Link
-                  className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Get Started
-                </Link>
-                <Link
-                  className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Learn more
-                </Link>
-              </div>
+              <h1 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl lg:text-5xl/none mb-5">
+                Mint & Redeem
+              </h1>
+              <h2 className="text-m sm:text-l md:text-xl lg:text-2xl/none">
+                Mint VUSD with various assets and redeem them for the underlying assets. 
+              </h2>
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col justify-center space-y-4">
-              <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
-                  DeltaNeutral Stablecoin
-                </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Where Traditional Finance Meets Web3
-                </h2>
-                <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                  DeltaNeutral Stablecoin combines the best of both worlds to provide a stable, reliable, and secure
-                  cryptocurrency for all your financial needs.
-                </p>
-              </div>
-              <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                <Link
-                  className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Learn More
-                </Link>
-                <Link
-                  className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="#"
-                >
-                  Contact Us
-                </Link>
-              </div>
-            </div>
+        <section className="w-5/6 mx-auto items-center sm:rounded-lg lg:py-2 bg-green-50">
+          <div className="flex flex-row container mx-auto px-4 md:px-6">
+            <MintBox />
           </div>
         </section>
       </main>
-  );
+    </>
+  )
 }
